ede376ec6388be987de3a304d503cc30d4529242
Steve Womble <swomble@aol.com>
2020-06-24 13:50:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,18 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    sql = "pragma table_info('#{table_name}')"
+    results = DB[:conn].execute(sql)
+    results.map do |column|
+      column['name']
+    end.compact
+  end
+  
+  
+  
 end
