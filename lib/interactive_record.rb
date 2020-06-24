51dd372a8d94e4d36468fae320486261fc783619
Steve Womble <swomble@aol.com>
2020-06-24 14:42:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#(send"
+      values << "'#{send(col_name)"
   end
   
 end
