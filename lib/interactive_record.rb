44d4fa58e0923b89d00c9fa352785e7bb5d0b04e
Steve Womble <swomble@aol.com>
2020-06-24 15:22:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,9 +55,6 @@ class InteractiveRecord
   end
   
   def self.find_by(attributes)
-    #column_name = attribute.keys[0].to_s
-    #value_name = attribute.values[0]
-    
     sql = "SELECT * FROM #{self.table_name} WHERE #{attributes.keys[0].to_s} = '#{attributes.values[0].to_s}'"
     
     DB[:conn].execute(sql)
